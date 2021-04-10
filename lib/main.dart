c61dccdc2c34dd81fb15d439d65afc19123f82e9
Jayeshthk <thakarejaydi18ce@student.mes.ac.in>
2021-04-10 22:02:26 +0530

    push

--- lib/main.dart
+++ lib/main.dart
@@ -407,10 +407,113 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       floatingActionButton: FloatingActionButton(
+        child: Builder(
+          builder: (context) => IconButton(
+            icon: Icon(Icons.lightbulb),
+            onPressed: () => Scaffold.of(context).openDrawer(),
+          ),
+        ),
         backgroundColor: Color(0xffffc107),
-        child: Icon(Icons.lightbulb),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 30,
+            ),
+            DrawerHeader(
+              child: Container(
+                height: 142,
+                width: MediaQuery.of(context).size.width,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {});
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                'Profile',
+                style: TextStyle(
+                  fontFamily: 'Avenir',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 45,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Text(
+                'Settings',
+                style: TextStyle(
+                  fontFamily: 'Avenir',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 45,
+            ),
+            Text(
+              'About',
+              style: TextStyle(
+                fontFamily: 'Avenir',
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 45,
+            ),
+            Text(
+              'Log Out',
+              style: TextStyle(
+                fontFamily: 'Avenir',
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 45,
+            ),
+            Expanded(
+                child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 65,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.black,
+                child: Center(
+                  child: Text(
+                    'v1.0.1',
+                    style: TextStyle(
+                      fontFamily: 'Avenir',
+                      fontSize: 20,
+                      color: const Color(0xffffffff),
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ))
+          ],
+        ),
+      ),
     );
   }
 }
